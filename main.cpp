3256cd8e5eb57aa8395a16c24c29b7430384c88e
Andrew Cash <andrewcash@Andrews-MacBook-Pro.local>
2018-03-11 04:01:36 -0500

    added functions to check for other letters of the alphabet

--- main.cpp
+++ main.cpp
@@ -18,8 +18,360 @@ void checkA(string s, bool &works)
     return;
 }
 
+void checkB(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'b') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkC(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'c') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkD(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'd') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkE(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'e') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkF(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'f') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkG(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'g') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkH(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'h') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkI(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'i') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkJ(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'j') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkK(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'k') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkL(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'l') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkM(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'm') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkN(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'n') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkO(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'o') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkP(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'p') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkQ(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'q') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkR(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'r') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkS(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 's') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkT(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 't') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkU(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'u') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkV(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'v') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkW(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'w') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkX(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'x') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkY(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'y') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
+void checkZ(string s, bool &works)
+{
+    int count = 0;
+
+    for (int i=0; i<s.size(); i++)
+    {
+        if (s[i] == 'z') count++;
+    }
+
+    if (count > 5) (works = false);
+
+    return;
+}
+
 int main()
 {
+    cout << "Please enter a phrase: ";
+
     string phrase;
     getline(cin, phrase);
 
@@ -27,6 +379,32 @@ int main()
     bool phraseWorks = true;
 
     checkA(phrase, phraseWorks);
+    checkB(phrase, phraseWorks);
+    checkC(phrase, phraseWorks);
+    checkD(phrase, phraseWorks);
+    checkE(phrase, phraseWorks);
+    checkF(phrase, phraseWorks);
+    checkG(phrase, phraseWorks);
+    checkH(phrase, phraseWorks);
+    checkI(phrase, phraseWorks);
+    checkJ(phrase, phraseWorks);
+    checkK(phrase, phraseWorks);
+    checkL(phrase, phraseWorks);
+    checkM(phrase, phraseWorks);
+    checkN(phrase, phraseWorks);
+    checkO(phrase, phraseWorks);
+    checkP(phrase, phraseWorks);
+    checkQ(phrase, phraseWorks);
+    checkR(phrase, phraseWorks);
+    checkS(phrase, phraseWorks);
+    checkT(phrase, phraseWorks);
+    checkU(phrase, phraseWorks);
+    checkV(phrase, phraseWorks);
+    checkW(phrase, phraseWorks);
+    checkX(phrase, phraseWorks);
+    checkY(phrase, phraseWorks);
+    checkZ(phrase, phraseWorks);
+
 
     if (phraseWorks)
         cout << "Go ahead bud.\n";
